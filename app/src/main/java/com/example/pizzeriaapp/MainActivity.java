144a17142ba8b75a603d8f7fd8ec481c75bbb4dc
Mervin-James <43912182+Mervin-James@users.noreply.github.com>
2021-11-29 20:33:41 -0500

    main screen UI finished and main controller init

--- app/src/main/java/com/example/pizzeriaapp/MainActivity.java
+++ app/src/main/java/com/example/pizzeriaapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.pizzeriaapp;
 
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView phoneNumberTf;
+    private ImageButton deluxeBtn, hawaiianBtn, pepperoniBtn,
+            currentOrdersBtn, storeOrdersBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phoneNumberTf = findViewById(R.id.customerNumberTextField);
+        deluxeBtn = findViewById(R.id.deluxeButton);
+        hawaiianBtn = findViewById(R.id.hawaiianButton);
+        pepperoniBtn = findViewById(R.id.pepperoniButton);
+        currentOrdersBtn = findViewById(R.id.currentOrdersButton);
+        storeOrdersBtn = findViewById(R.id.storeOrdersButton);
     }
+
+    public void onDeluxeClick(View view) {
+
+    }
+
+    public void onHawaiianClick(View view) {
+
+    }
+
+    public void onPepperoniClick(View view) {
+
+    }
+
+    public void onCurrentOrdersClick(View view) {
+
+    }
+
+    public void onStoreOrdersClick(View view) {
+
+    }
+
+
+
 }
